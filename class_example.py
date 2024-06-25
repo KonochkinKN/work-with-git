47e595f4cb38a533edf64b9e30645805dd72a94d
topteacher-py <topzelegrad202301@gmail.com>
2024-06-25 20:59:44 +0300

    Classes. Lesson 2

--- class_example.py
+++ class_example.py
@@ -2,54 +2,63 @@ import random
 
 
 class Car:
-    def __init__(self, brand, model,
-                 year=2000, color=(0, 0, 255)):
+    __color = (0, 0, 255)
+    __year = 2000
+    __condition = {'tires': 100,
+                   'engine': 100,
+                   'fuel': 100,
+                   'glass': 100,
+                   'body': 100}
+
+    def __init__(self, brand, model):
         """ инициализация """
         self.brand = brand
         self.model = model
-        self.year = year
-        self.color = color
-        self.condition = {'tires': 100,
-                          'engine': 100,
-                          'fuel': 100,
-                          'glass': 100,
-                          'body': 100}
+
+    def change_year(self, year):
+        if 1970 <= year <= 2024:
+            self.__year = year
 
     def change_color(self, new_color):
         """ перекрасить """
-        self.color = new_color
+        if all([0 <= c <= 255 for c in new_color]):
+            self.__color = new_color
 
     def change_condition(self, part, value):
-        if part in self.condition.keys():
-            self.condition[part] = value
+        if value > 100 or value < 0:
+            print('invalid value:', value)
+            return
+        if part in self.__condition.keys():
+            self.__condition[part] = value
         else:
             print('invalid part:', part)
 
     def show_condition(self):
         print('Condition:')
-        for part in self.condition.keys():
-            print(f'{part}:\t{self.condition[part]}')
+        for part in self.__condition.keys():
+            print(f'{part}:\t{self.__condition[part]}')
 
     def crash(self, level):
-        print(f'crash level {level}')
+        print(f'crash level: {level}')
         if level == 'low':
-            self.condition['body'] -= random.randint(5, 20)
-            self.condition['glass'] -= random.randint(0, 10)
+            self.__condition['body'] -= random.randint(5, 20)
+            self.__condition['glass'] -= random.randint(0, 10)
         elif level == 'medium':
-            self.condition['body'] -= random.randint(10, 40)
-            self.condition['glass'] -= random.randint(5, 30)
-            self.condition['engine'] -= random.randint(0, 10)
+            self.__condition['body'] -= random.randint(10, 40)
+            self.__condition['glass'] -= random.randint(5, 30)
+            self.__condition['engine'] -= random.randint(0, 10)
         elif level == 'high':
-            self.condition['body'] -= random.randint(30, 80)
-            self.condition['glass'] -= random.randint(30, 80)
-            self.condition['engine'] -= random.randint(10, 50)
+            self.__condition['body'] -= random.randint(30, 80)
+            self.__condition['glass'] -= random.randint(30, 80)
+            self.__condition['engine'] -= random.randint(10, 50)
         elif level == 'total':
-            self.condition['body'] -= random.randint(50, 95)
-            self.condition['glass'] -= random.randint(50, 95)
-            self.condition['engine'] -= random.randint(30, 70)
+            self.__condition['body'] -= random.randint(50, 95)
+            self.__condition['glass'] -= random.randint(50, 95)
+            self.__condition['engine'] -= random.randint(30, 70)
 
     def __str__(self):
-        return f'{self.brand} {self.model}, color: {self.color}'
+        return f'{self.brand} {self.model},' \
+               f' color: {self.__color}, year: {self.__year}'
 
 
 class Kia(Car):
@@ -62,7 +71,7 @@ class KiaRio(Kia):
         super().__init__('Rio', year, color)
 
 
-if __name__ == '__main__':
+def test():
     # ford = Car(brand='Ford', model='Focus', year=2005)
     # print(ford)
     # ford.change_color((255, 0, 0))
@@ -84,3 +93,9 @@ if __name__ == '__main__':
     print(isinstance(kia_rio, Car))
 
 
+if __name__ == '__main__':
+    ford = Car(brand='Ford', model='Focus')
+    ford.change_color((0, 0, 0))
+    print(ford)
+    ford.change_condition('engine', 80)
+    ford.show_condition()

ebefa70b18801d27ab72bd33efd712bbb62b3d89
topteacher-py <topzelegrad202301@gmail.com>
2024-07-11 21:20:45 +0300

    Провел второй урок по структурам данных

--- utils/data/list_structure.py
+++ utils/data/list_structure.py
@@ -8,10 +8,44 @@ class List:
         if self.next is not None:
             list_string += '|\n' + self.next.__str__()
         return list_string
-    
+
+    def __len__(self):
+        length = 1
+        if self.next:
+            length += len(self.next)
+        return length
+
+    def index(self, data, ind=0):
+        if self.data == data:
+            return ind
+        elif self.next:
+            return self.next.index(data, ind+1)
+        else:
+            return None
+
+    def __getitem__(self, ind):
+        if ind == 0:
+            return self.data
+        elif ind > 0 and self.next:
+            return self.next[ind-1]
+        else:
+            return None
+
+    def __setitem__(self, ind, data):
+        if ind == 0:
+            self.data = data
+        elif ind > 0 and self.next:
+            self.next[ind-1] = data
+
+
 e1 = List('first')
 e2 = List('second')
 e3 = List('third')
 e2.next = e3
 e1.next = e2
+print(e1)
+print(len(e1))
+print(e1.index('third'))
+print(e1[2])
+e1[2] = '3rd'
 print(e1)
--- utils/data/tree_structure.py
+++ utils/data/tree_structure.py
@@ -1,18 +1,67 @@
 class Tree:
     def __init__(self, data):
         self.children = []
-        self.data = data    
+        self.data = data
 
     def __str__(self, shift=0):
         tree_string = ''
         if shift > 1:
-            tree_string += '|   ' * (shift-1)
+            tree_string += '    ' * (shift-1)
         if shift > 0:
             tree_string += '|___'
         tree_string += f'{self.data}\n'
         for child in self.children:
             tree_string += child.__str__(shift+1)
         return tree_string
+
+    def index(self, data, address='/'):
+        if self.data == data:
+            return address
+        elif len(self.children):
+            new_address = address + f'{self.data}/'
+            for child in self.children:
+                addr = child.index(data, new_address)
+                if addr:
+                    return addr
+        return None
+
+    def __getitem__(self, address):
+        splitted = address.split('/')
+        splitted = [s for s in splitted if s]
+        new_addr = '/'.join(splitted[1:]) + '/'
+        cur_addr = splitted[0]
+        if self.data != cur_addr:
+            return None
+        if new_addr == '/':
+            return self
+        else:
+            for child in self.children:
+                data = child[new_addr]
+                if data:
+                    return data
+
+    def __setitem__(self, address, new_tree):
+        splitted = address.split('/')
+        splitted = [s for s in splitted if s]
+        new_addr = '/'.join(splitted[1:]) + '/'
+        cur_addr = splitted[0]
+        if self.data != cur_addr:
+            return
+        if new_addr == '/':
+            # FIXME
+            return self
+        else:
+            for child in self.children:
+                data = child[new_addr]
+                if data:
+                    return data
+
+    def __len__(self):
+        depth = 1
+        max_child_depth = 0
+        for child in self.children:
+            max_child_depth = max(max_child_depth, len(child))
+        return depth + max_child_depth
 
 
 root = Tree("root")
@@ -25,3 +74,8 @@ right.children = [Tree('right1'), middle]
 root = Tree("root")
 root.children = [left, middle, right]
 print(root)
+# print(f'{root.index("left2") = }')
+print(root['/root/right/'])
+print(len(root))
+
+
